5c6310cd9cfdd7f92d1f6ca2c96d5fa01aa37907
Ani Dwi <anidwi.ad1@gmail.com>
2021-07-10 22:38:57 +0700

    faskes layout

---
+++ app/src/main/java/com/awrcorp/tbmapping/FaskesActivity.java
@@ -0,0 +1,14 @@
+package com.awrcorp.tbmapping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaskesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faskes);
+    }
+}
