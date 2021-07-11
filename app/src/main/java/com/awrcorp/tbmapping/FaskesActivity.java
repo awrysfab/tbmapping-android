4cc301abee0c197cac298e235039c481257070ec
Ani Dwi <anidwi.ad1@gmail.com>
2021-07-11 14:16:01 +0700

    onclick

--- app/src/main/java/com/awrcorp/tbmapping/FaskesActivity.java
+++ app/src/main/java/com/awrcorp/tbmapping/FaskesActivity.java
@@ -2,13 +2,24 @@ package com.awrcorp.tbmapping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FaskesActivity extends AppCompatActivity {
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faskes);
+
+        back = (ImageView) findViewById(R.id.faskes_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
