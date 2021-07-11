f7609327e35cf8e06886ba7a39958ac676d71989
Ani Dwi <anidwi.ad1@gmail.com>
2021-07-11 14:15:48 +0700

    onclick

--- app/src/main/java/com/awrcorp/tbmapping/InfoActivity.java
+++ app/src/main/java/com/awrcorp/tbmapping/InfoActivity.java
@@ -2,13 +2,60 @@ package com.awrcorp.tbmapping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class InfoActivity extends AppCompatActivity {
+    ImageView back;
+    LinearLayout pengenalan, gejala, pencegahan, pengobatan;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        pengenalan = (LinearLayout) findViewById(R.id.info_pengenalan);
+        gejala = (LinearLayout) findViewById(R.id.info_gejala);
+        pencegahan = (LinearLayout) findViewById(R.id.info_pencegahan);
+        pengobatan = (LinearLayout) findViewById(R.id.info_pengobatan);
+
+        back = (ImageView) findViewById(R.id.info_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                 finish();
+            }
+        });
+
+        pengenalan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intent = new Intent(getApplicationContext(), PengenalanActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gejala.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intent = new Intent(getApplicationContext(), GejalaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        pencegahan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intent = new Intent(getApplicationContext(), PencegahanActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        pengobatan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intent = new Intent(getApplicationContext(), PengobatanActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/awrcorp/tbmapping/MainActivity.java
+++ app/src/main/java/com/awrcorp/tbmapping/MainActivity.java
@@ -2,13 +2,52 @@ package com.awrcorp.tbmapping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout menuPeta, menuInfo, menuNews, menuFaskes;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menuPeta = (LinearLayout) findViewById (R.id.menu_peta);
+        menuInfo = (LinearLayout) findViewById (R.id.menu_info);
+        menuNews = (LinearLayout) findViewById (R.id.news_menu);
+        menuFaskes = (LinearLayout) findViewById (R.id.faskes_menu);
+
+//        menuPeta.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                 intent = new Intent(getApplicationContext(), PetaActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        menuInfo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intent = new Intent(getApplicationContext(), InfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        menuNews.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                intent = new Intent(getApplicationContext(), NewsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        menuFaskes.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intent = new Intent(getApplicationContext(), FaskesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
